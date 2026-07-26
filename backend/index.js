63e27b8702a51c14ca10c9cf1d42351056f4728b
Rudraksh <rudrakshmishra026@gmail.com>
2026-07-26 12:14:44 +0530

    vercel configuration

--- backend/index.js
+++ backend/index.js
@@ -55,6 +55,11 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
